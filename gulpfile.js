8d165d42b143b4d1b56d965bbd21e22f20fa84e8
dai <chamestica@gmail.com>
2016-01-16 03:09:30 +0900

    Use gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const
+  gulp = require('gulp'),
+  gulpLoadPlugins = require('gulp-load-plugins'),
+
+  $ = gulpLoadPlugins();
+
+/**
+ *  デフォルトタスク
+ */
+gulp.task('default', () => {});
