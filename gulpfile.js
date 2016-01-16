aacc810b065cd60546d3be00c3222a41e488049a
dai <chamestica@gmail.com>
2016-01-16 16:29:53 +0900

    Use Browserify with Babelify

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@
 const
   gulp = require('gulp'),
   gulpLoadPlugins = require('gulp-load-plugins'),
+  browserify = require('browserify'),
+  babelify = require('babelify'),
+  source = require('vinyl-source-stream'),
   conf = require('./gulp-config'),
 
   $ = gulpLoadPlugins();
@@ -19,6 +22,17 @@ gulp.task('js:lint', () => {
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.eslint.failOnError());
+});
+
+/**
+ *  Browserify(Babelify)
+ */
+gulp.task('js:bundle', ['js:lint'], () => {
+  browserify(`${conf.dir.src}${conf.dir.js}main.js`, {debug: true})
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(`${conf.dir.dst}${conf.dir.js}`));
 });
 
 /**
