07334dbea5447eacbc817e1efb148fdfb820b1a3
dai <chamestica@gmail.com>
2016-01-16 14:28:15 +0900

    Use ESLint

---
+++ gulp-config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  dir: {
+    src: 'src/',
+    dst: 'public/',
+    css: 'stylesheets/',
+    js: 'javascripts/',
+    map: 'maps/'
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,23 @@
 const
   gulp = require('gulp'),
   gulpLoadPlugins = require('gulp-load-plugins'),
+  conf = require('./gulp-config'),
 
   $ = gulpLoadPlugins();
+
+/**
+ *  ESLint
+ */
+gulp.task('js:lint', () => {
+  return gulp
+    .src([
+      `${conf.dir.src}${conf.dir.js}**/*.js`,
+      'gulpfile.js'
+    ])
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failOnError());
+});
 
 /**
  *  デフォルトタスク
