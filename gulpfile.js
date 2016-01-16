3360e0208ef5fccbb0a5f60d89816e7ce4c2cafa
dai <chamestica@gmail.com>
2016-01-16 19:10:37 +0900

    Remove the unnecessary task

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ const
   babelify = require('babelify'),
   source = require('vinyl-source-stream'),
   buffer = require('vinyl-buffer'),
-  del = require('del'),
   fs = require('fs'),
   conf = require('./gulp-config'),
 
@@ -59,20 +58,6 @@ gulp.task('js:bundle', ['js:lint'], (done) => {
     .pipe($.rename({suffix: '.min'}))
     .pipe($.if(conf.debug, $.sourcemaps.write(`../${conf.dir.map}`)))
     .pipe(gulp.dest(`${conf.dir.dst}${conf.dir.js}`));
-});
-
-/**
- *  minify前のjsを削除
- */
-gulp.task('js:clean', ['js:bundle'], () => {
-  return del([
-    `${conf.dir.dst}${conf.dir.js}**/*.js`,
-    `!${conf.dir.dst}${conf.dir.js}**/*.min.js`
-  ], (err, paths) => {
-    if (err) {
-      console.log(err, paths);
-    }
-  });
 });
 
 /**
