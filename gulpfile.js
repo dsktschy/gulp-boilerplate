169912c2ca1b5f81499c9e2f1cc227c3d613c9d1
dai <chamestica@gmail.com>
2016-02-23 02:20:36 +0900

    Use require-dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,10 @@
 'use strict';
 
-require('./gulp/tasks/html');
-require('./gulp/tasks/html/lint');
-require('./gulp/tasks/html/min');
-require('./gulp/tasks/html/reload');
-require('./gulp/tasks/css');
-require('./gulp/tasks/css/lint');
-require('./gulp/tasks/css/min');
-require('./gulp/tasks/css/reload');
-require('./gulp/tasks/js');
-require('./gulp/tasks/js/lint');
-require('./gulp/tasks/js/min');
-require('./gulp/tasks/js/reload');
-require('./gulp/tasks/image');
-require('./gulp/tasks/image/min');
-require('./gulp/tasks/image/reload');
-require('./gulp/tasks/etc');
-require('./gulp/tasks/etc/copy');
-require('./gulp/tasks/etc/reload');
-require('./gulp/tasks/watch');
+const
+  gulp = require('gulp'),
+  requireDir = require('require-dir');
 
-const gulp = require('gulp');
+requireDir('./gulp/tasks', {recurse: true});
 
 /**
  *  デフォルトタスク
